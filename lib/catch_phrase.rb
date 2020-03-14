c32b35b407d7288d183e5d8079a7044e912e56b5
Nicole Sims <nssims105@gmail.com>
2020-03-14 14:52:53 +0000

    Done.

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,3 +1,18 @@
-def catch_phrase
-  puts phrase
+#Define a method that takes in an argument and pass a 
+variable in as that argument.
+def spec_helper (catch_phrase) do
+  puts catch_phrase 
+catch_phrase = It's-a me, Mario!"
+spec_helper
+
+# def catch_phrase (phrase)
+#   puts "catchy #{phrase}"
+# end
+
+def catch_phrase(phrase="It's s-a me, Mario)
+  puts "#{phrase}"
 end
+
+catch_phrase
+
+
